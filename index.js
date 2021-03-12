2023dbed1c1da3ba6c9ecda33219afd0f9fe77fe
Borja <borjaneumann@hotmail.com>
2021-03-12 21:18:18 +0100

    Layout update and the api's functionalities have been added

--- index.js
+++ index.js
@@ -3,32 +3,39 @@
 //  one location for the result of the search
 //  one location for the picture of the city that is searched.
 //establish api connection through fetch and check if information is received.
-//      for the weather data I will use open weather api
-//      for the city image 
+//      Weather data from Open Weather API
+//      Image data from Pexels API
 
 
 var weather_key =config.weatherApi_key;
 var picture_key =config.pictureApi_key;
 
-document.getElementById('test');
-let cityText = document.getElementById('response');
-let cityQuery = document.getElementById('query');
-
-
 document.getElementById('request').addEventListener('click',getExternalData);
 
 function getExternalData(){
     let cityInput = document.getElementById('cityName').value;
+    let cityImage = document.getElementById('cityImage');
+    let temp = document.getElementById('temp');
+    let tempFeel = document.getElementById('tempFeel');
+    let tempMin = document.getElementById('tempMin');
+    let tempMax = document.getElementById('tempMax');
+    let pressure = document.getElementById('pressure');
+    let humidity = document.getElementById('humidity');
     
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${weather_key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${weather_key}&units=metric`)
     .then(function(response){
         return response.json()})
     .then(function(data){
         console.log(data)
-        cityText.innerHTML=data.name;
-        cityQuery.innerHTML=Math.round(parseFloat(data.main.temp)-273.15) ; 
-    });
+        temp.innerHTML=Math.round(data.main.temp)+"°C" ;
+        tempFeel.innerHTML=Math.round(data.main.feels_like)+"°C";
+        tempMin.innerHTML=Math.round(data.main.temp_min)+"°C";
+        tempMax.innerHTML= Math.round(data.main.temp_max)+"°C";
+        pressure.innerHTML=data.main.pressure+"hPa";
+        humidity.innerHTML=data.main.humidity+"%";
+    })
+    .catch(err=>M.toast({html: 'mmm...maybe a typo or the city name doesn\'t exist by our data base!'}))
 
     fetch(`https://api.pexels.com/v1/search?query=${cityInput}`,{
         headers: {
@@ -38,17 +45,17 @@ function getExternalData(){
     .then(function(response){
         return response.json()})
     .then(function(data){
-        console.log(data)
-         ; 
-    });
+        console.log(data);
+        if (cityInput=="Yokohama"||cityInput=="yokohama"){
+            cityImage.src=data.photos[2].src.landscape;
+        }else{
+            cityImage.src=data.photos[0].src.landscape;
+        }
+    })
+    .catch(err=>M.toast({html: 'mmm...something went wrong!'}))
+
 }
 
 
 
 
-    // import { createClient } from 'pexels';
-
-    // const client = createClient('YOUR_API_KEY');
-    // const query = 'Nature';
-    
-    // client.photos.search({ query, per_page: 1 }).then(photos => {...});
